b3bc591034952db469ff1097b5995d03d78e3770
maauyu <159198030+maauyu@users.noreply.github.com>
2024-03-13 10:57:24 -0700

    L3

--- 04.to_space_and_beyond_workbook/practices/L3.P2.character_diagonals/character_diagonals.gd
+++ 04.to_space_and_beyond_workbook/practices/L3.P2.character_diagonals/character_diagonals.gd
@@ -11,8 +11,8 @@ func _process(delta: float) -> void:
 	direction.x = Input.get_axis("move_left", "move_right")
 	direction.y = Input.get_axis("move_up", "move_down")
 
-	# The character is way too fast, but only when moving diagonally!
-	# Add code to prevent that.
+	if direction.length() > 1.0:
+		direction = direction.normalized()
 
 	velocity = direction * max_speed
 	position += velocity * delta
